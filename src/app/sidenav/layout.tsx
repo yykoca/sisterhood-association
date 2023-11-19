3cf9eccc226069506d88c2ec0aa0c23a9d3b1088
Yahya Yasir Koca <yykoca.08@gmail.com>
2023-11-19 15:56:52 +0100

    Remove unnecessary sidenav component

--- src/app/sidenav/layout.tsx
+++
@@ -1,7 +0,0 @@
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="flex h-screen flex-col md:flex-col md:overflow-hidden">
-      <div className="w-full flex-none md:w-64">{children}</div>
-    </div>
-  );
-}
