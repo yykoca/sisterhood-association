78d95adced3fdd636aee6fa00eed9683ef3dc7e3
Yahya Yasir Koca <yykoca.08@gmail.com>
2023-11-19 15:57:43 +0100

    Add some style to sidenav

--- src/app/ui/sidenav.tsx
+++ src/app/ui/sidenav.tsx
@@ -4,24 +4,27 @@ import Image from 'next/image';
 
 export default function SideNav() {
   return (
-    <div className="flex h-full flex-row p-4">
-      <Link
-        className="flex items-center justify-start rounded-md"
-        href="/"
-      >
-        <div className="w-100 md:w-40 flex items-center justify-center">
-          <Image
-              src="/images/adz-logo-blue.png"
-              alt="Logo"
-              width={100}
-              height={100}
-              priority
-            />
+    <div className='absolute w-full bg-white bg-opacity-80 z-10'>
+      <div className="flex flex-row xl:container mx-auto p-4">
+        <Link
+          className="flex items-center justify-start rounded-md"
+          href="/"
+        >
+          <div className="w-100 md:w-40 flex items-center justify-center">
+            <Image
+                className="xl:ms-1 ms-6"
+                src="/images/adz-logo-blue.png"
+                alt="Logo"
+                width={100}
+                height={100}
+                priority
+              />
+          </div>
+        </Link>
+        <div className="flex grow flex-row justify-between items-center space-x-2 md:flex-row md:space-x-0 md:space-y-2">
+          <div className="hidden h-auto w-full grow rounded-md md:block"></div>
+          <NavLinks />
         </div>
-      </Link>
-      <div className="flex grow flex-row justify-between space-x-2 md:flex-row md:space-x-0 md:space-y-2">
-        <div className="hidden h-auto w-full grow rounded-md md:block"></div>
-        <NavLinks />
       </div>
     </div>
   );
